0f4b8f91e06edb2744fa396690ebd890b2f25b1d
GiovannyGarzon <giovankyandres@gmail.com>
2023-04-13 00:03:24 -0400

    Sqlite && typeOrm setup

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from './post.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(Post) private postsRepository: Repository<Post>,
+  ) {}
+
+  async findAll(): Promise<Post[]> {
+    return await this.postsRepository.find();
+  }
+}
